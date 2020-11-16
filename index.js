adaf186e9761b6ce0482e9aece21b38bfa48a587
NataliaSilantieva <63507488+NataliaSilantieva@users.noreply.github.com>
2020-11-16 20:18:14 +0200

    Update index.js

--- index.js
+++ index.js
@@ -7,33 +7,34 @@ const colors = [
     '#795548',
   ];
    const min = 0;
-   const max = colors.length -1; 
-   
+   const max = colors.length -1;
+   let intervalId = ''; 
    
   const startBtn = document.querySelector('[data-action="start"]');
   const stopBtn = document.querySelector('[data-action="stop"]');
   stopBtn.disabled = true;
-  startBtn.addEventListener('click', changeBodyColor); 
-  stopBtn.addEventListener('click', stopChangeBodyColor);
+  startBtn.addEventListener('click', start); 
+  stopBtn.addEventListener('click', stop);
+  
   const randomIntegerFromInterval = (min, max) => {
-    
-    return   Math.floor(Math.random() * (max - min + 1) + min);
-    
+    return   Math.floor(Math.random() * (max - min + 1) + min); 
   };
 
- 
-  
-  function changeBodyColor  ()  { 
-    
+
+  function changeBodyColor() {
     document.body.style.backgroundColor = colors[randomIntegerFromInterval(min,max)];
+  };
+  
+
+  function start  ()  { 
     startBtn.disabled = true;
     stopBtn.disabled = false;
+     intervalId = setInterval(changeBodyColor, 1000);
     
   };
   
-  let intervalId = setInterval(changeBodyColor, 1000);
-
-  function stopChangeBodyColor  ()  {
+  
+  function stop  ()  {
     clearInterval(intervalId); 
     startBtn.disabled = false;
     stopBtn.disabled = true;
